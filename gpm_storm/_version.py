760d264bce1da4f292c451a13e4dcaca4de1a0d4
ghiggi <gionata.ghiggi@gmail.com>
2023-11-15 14:25:07 +0100

    Resolve Lorenzo Comi BUGS BUGS BUGS

--- gpm_storm/_version.py
+++ gpm_storm/_version.py
@@ -3,7 +3,6 @@
 TYPE_CHECKING = False
 if TYPE_CHECKING:
     from typing import Tuple, Union
-
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
     VERSION_TUPLE = object
@@ -13,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev2+gb93b3b3.d20231031"
-__version_tuple__ = version_tuple = (0, 1, "dev2", "gb93b3b3.d20231031")
+__version__ = version = '0.1.dev10+g403eeb7.d20231115'
+__version_tuple__ = version_tuple = (0, 1, 'dev10', 'g403eeb7.d20231115')
--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-
 from setuptools import setup
 
-setup(  name='gpm_storm',
-    version='1.0.0',
-    packages=find_packages(),)
+setup()
