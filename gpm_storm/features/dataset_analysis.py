035994af139b4bec4f0589cdabbe4473a211cf40
lorcomi <lorenzo.comi@hotmail.com>
2024-01-05 10:47:57 +0100

    new plots

--- gpm_storm/features/dataset_analysis.py
+++ gpm_storm/features/dataset_analysis.py
@@ -35,7 +35,7 @@ def _relative_distribution_of_dataset(df):
         if pd.api.types.is_numeric_dtype(df[column]) and column != ('time', 'along_track_start', 'along_track_stop', 'gpm_granule_id'):
             sns.histplot(data=df, x=column, kde=True, ax=axes[i], stat='percent', common_norm=False)
             axes[i].set_title(f"Relative Distribution of {column}")
-            axes[i].set_xlabel("Values")
+            axes[i].set_xlabel(f"Values ({units.get(column, '')})")
             axes[i].set_ylabel("Relative Frequency (%)")
 
     # Hide any empty subplots
@@ -66,7 +66,7 @@ def _relative_log_distribution_of_dataset(df):
             # Use log scale on the x-axis
             sns.histplot(data=df, x=column, kde=True, ax=axes[i], stat='percent', common_norm=False, log_scale=(True, False))
             axes[i].set_title(f"Relative Distribution of {column}")
-            axes[i].set_xlabel("Values (log scale)")
+            axes[i].set_xlabel(f"Values Log-scale ({units.get(column, '')})")
             axes[i].set_ylabel("Relative Frequency (%)")
 
     # Hide any empty subplots
@@ -97,7 +97,7 @@ def _boxplot_of_dataset(df):
         if pd.api.types.is_numeric_dtype(df[column]) and column != ('time', 'along_track_start', 'along_track_stop', 'gpm_granule_id'):
             sns.boxplot(x=column, data=df, ax=axes[i], showfliers=False)
             axes[i].set_title(f"Boxplot of {column}")
-            axes[i].set_xlabel("Values")
+            axes[i].set_xlabel(f"Values ({units.get(column, '')})")
     
     # Hide any empty subplots
     for i in range(num_columns, len(axes)):
@@ -123,21 +123,21 @@ def _bivariate_analysis(df, x_variable, y_variable, color_variable):
 
 
 
-    # Set the style of seaborn
-    sns.set(style="whitegrid")
+    # # Set the style of seaborn
+    # sns.set(style="whitegrid")
     
-    # Create a scatter plot with color based on the third variable
-    plt.figure(figsize=(10, 8))
-    sns.scatterplot(x=x_values, y=y_values, hue=color_values, palette='viridis', edgecolor='w', s=100)
+    # # Create a scatter plot with color based on the third variable
+    # plt.figure(figsize=(10, 8))
+    # sns.scatterplot(x=x_values, y=y_values, hue=color_values, palette='viridis', edgecolor='w', s=100)
     
-    # Add labels and a legend
-    plt.title(f'Bivariate Analysis of {x_variable} and {y_variable} (Colored by {color_variable})')
-    plt.xlabel(x_variable)
-    plt.ylabel(y_variable)
-    plt.legend(title=color_variable)
+    # # Add labels and a legend
+    # plt.title(f'Bivariate Analysis of {x_variable} and {y_variable} (Colored by {color_variable})')
+    # plt.xlabel(x_variable)
+    # plt.ylabel(y_variable)
+    # plt.legend(title=color_variable)
     
-    # Show the plot
-    plt.show()
+    # # Show the plot
+    # plt.show()
     
     bins = 20
     
@@ -149,17 +149,22 @@ def _bivariate_analysis(df, x_variable, y_variable, color_variable):
         statistic='mean',
         bins=bins
     )
-    
+
     # Create a heatmap using Seaborn
     plt.figure(figsize=(10, 8))
-    sns.heatmap(statistic.T, cmap='viridis', xticklabels=x_edges, cbar=True)
-    
- 
+    heatmap = sns.heatmap(statistic.T, cmap='viridis', cbar=True)
+
+    # Customize x-axis and y-axis ticks with rotation
+    x_tick_labels = [f'{val:.2f}' for val in x_edges[:-1]]
+    y_tick_labels = [f'{val:.2f}' for val in y_edges[:-1]]
+    heatmap.set_xticklabels(x_tick_labels, rotation=45, ha='right')  # Rotate x-axis labels by 45 degrees
+    heatmap.set_yticklabels(y_tick_labels, rotation=0, ha='right')   # Adjust rotation as needed for y-axis labels
+
     # Add labels and a title
     plt.title(f'Bivariate Analysis with Mean Values (Color Coded for {color_variable})')
     plt.xlabel(x_variable)
     plt.ylabel(y_variable)
-    
+
     # Show the plot
     plt.show()
 
@@ -280,7 +285,7 @@ def preliminary_dataset_analysis(dst_dir):
     
     _relative_distribution_of_dataset(df)
     _boxplot_of_dataset(df)
-    _bivariate_analysis(df, 'precipitation_pixel', 'precipitation_sum', 'lat')
+    _bivariate_analysis(df, 'precipitation_average', 'REFCH_mean', 'precipitation_pixel')
     _relative_log_distribution_of_dataset(df)
     results = _normality_tests(df_no_nan)
     
