cddaed4c3d65d2fd5f45729e1ce924a86eafcf07
lorcomi <lorenzo.comi@hotmail.com>
2024-01-06 18:24:33 +0100

    plots

--- gpm_storm/som/experiments.py
+++ gpm_storm/som/experiments.py
@@ -23,7 +23,37 @@ def get_experiment_info(name):
                     'percentage_rainy_pixels_between_5_and_10',
                     'percentage_rainy_pixels_between_10_and_20','REFCH_mean','REFCH_max', 'echodepth18_mean', 'echodepth18_max'],
             "som_grid_size": (10, 10)
-           }
+           },
+        "shape_SOM": {
+            "features": ['precipitation_average', 'precipitation_std', 'precipitation_pixel',
+                   'center_precipitation_pixel', 'precipitation_sum', 'precipitation_max',
+                   'count_rainy_areas_over_0', 'mean_for_rainy_pixels_over_0',
+                   'count_rainy_pixels_over_0', 'major_axis_largest_patch_over_0',
+                   'minor_axis_largest_patch_over_0', 'aspect_ratio_largest_patch_over_0',
+                   'count_rainy_pixels_in_patch_over_0', 'count_rainy_areas_over_1',
+                   'mean_for_rainy_pixels_over_1', 'count_rainy_pixels_over_1',
+                   'major_axis_largest_patch_over_1', 'minor_axis_largest_patch_over_1',
+                   'aspect_ratio_largest_patch_over_1',
+                   'count_rainy_pixels_in_patch_over_1', 'count_rainy_areas_over_2',
+                   'mean_for_rainy_pixels_over_2', 'count_rainy_pixels_over_2',
+                   'major_axis_largest_patch_over_2', 'minor_axis_largest_patch_over_2',
+                   'aspect_ratio_largest_patch_over_2',
+                   'count_rainy_pixels_in_patch_over_2', 'count_rainy_areas_over_5',
+                   'mean_for_rainy_pixels_over_5', 'count_rainy_pixels_over_5',
+                   'major_axis_largest_patch_over_5', 'minor_axis_largest_patch_over_5',
+                   'aspect_ratio_largest_patch_over_5',
+                   'count_rainy_pixels_in_patch_over_5', 'count_rainy_areas_over_10',
+                   'mean_for_rainy_pixels_over_10', 'count_rainy_pixels_over_10',
+                   'major_axis_largest_patch_over_10', 'minor_axis_largest_patch_over_10',
+                   'aspect_ratio_largest_patch_over_10',
+                   'count_rainy_pixels_in_patch_over_10',
+                   'percentage_rainy_pixels_between_0_and_1',
+                   'percentage_rainy_pixels_between_1_and_2',
+                   'percentage_rainy_pixels_between_2_and_5',
+                   'percentage_rainy_pixels_between_5_and_10',
+                   'percentage_rainy_pixels_between_10_and_20'],
+                "som_grid_size": (10, 10)
+            }
 
     }
     return info_experiment[name]
